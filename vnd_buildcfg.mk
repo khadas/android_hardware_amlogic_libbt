5c85dac6030b183454d1ecf0a8455e8be60f4131
jiamin.miao <jiamin.miao@amlogic.com>
2013-05-27 16:50:23 +0800

    pd#74111:enable BT LPM function

--- vnd_buildcfg.mk
+++ vnd_buildcfg.mk
@@ -1,6 +1,10 @@
 intermediates := $(local-intermediates-dir)
 
+ifeq ($(BCM_BLUETOOTH_LPM_ENABLE),true)
+SRC := $(call my-dir)/include/vnd_40183_lpm.txt
+else
 SRC := $(call my-dir)/include/vnd_40183.txt
+endif
 ifeq (,$(wildcard $(SRC)))
 # configuration file does not exist. Use default one
 SRC := $(call my-dir)/include/vnd_generic.txt
